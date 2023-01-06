671bbcc89d84caea1bdd07b7c3efcc7bc29e79f6
Denkata-Iliev <denislav9711@gmail.com>
2023-01-06 16:20:46 +0200

    Modify UpdateUserRequest for PATCH-like update of users

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Requests;
 
-use App\Actions\Fortify\PasswordValidationRules;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
 
 class UpdateUserRequest extends FormRequest
 {
-    use PasswordValidationRules;
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -27,9 +25,10 @@ class UpdateUserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => $this->passwordRules()
+            'name' => 'nullable|string|max:255',
+            'email' => 'nullable|string|email|max:255',
+            'password' => 'nullable|string|min:8',
+            'role' => 'nullable'
         ];
     }
 }
