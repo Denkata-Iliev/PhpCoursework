9a66507468617fd7c7b722d7f563555947303b2b
Denkata-Iliev <denislav9711@gmail.com>
2023-01-08 18:26:48 +0200

    Create Room seeder for db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
            RolesAndPermissionsSeeder::class,
-           AdminSeeder::class
+           AdminSeeder::class,
+            RoomsSeeder::class
         ]);
     }
 }
